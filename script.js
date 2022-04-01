458552970bd9513ff6f9c5352e5e7e01a0e2e249
Thiago <thiagosilvavr2013@gmail.com>
2022-03-31 22:37:22 -0300

    index.html

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const Game = () => {
+    let display = document.getElementById('display');
+    let button = document.getElementById('button');
+    let randomBtn = document.getElementById('random');
+    let invincicleBtn = document.getElementById('invincible');
 
     let possiblePlay = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let computerPlay = [];
@@ -17,14 +21,37 @@ const Game = () => {
 
     let winComb = [[1,4,7], [2,5,8], [3,6,9], [1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7]];
 
+    let difficulty = 1;
+    
+    //Reset
+    button.addEventListener('click', function() {
+        square().forEach(el => {
+            el.div.textContent = '';
+        })
+        checkWin().length = 0;
+        playerPlay.length = 0;
+        computerPlay.length = 0;
+        possiblePlay = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        winComb = [[1,4,7], [2,5,8], [3,6,9], [1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7]];
+        ai.length = 0;
+        display.textContent = ' ';
+    })
+
+    randomBtn.addEventListener('click', function() {
+        difficulty = 1;
+    })
+
+    invincicleBtn.addEventListener('click', function() {
+        difficulty = 2;
+    })
+
     const checkWin = () => {
         let winner = [];
         //Check if computer win;        
         winComb.forEach(el => {
             let checker = () => el.every(v => computerPlay.includes(v));
             if (checker() === true && winner.length === 0) {
-                winner.push('O computador Venceu');
-                return
+                winner.push('You lose.');
             }
         })
 
@@ -32,11 +59,69 @@ const Game = () => {
         winComb.forEach(el => {
             let checker = () => el.every(v => playerPlay.includes(v));
             if (checker() === true && winner.length === 0) {
-                winner.push('Você venceu!');
-                return
+                winner.push('You win!');
             }
         })
+        display.textContent = winner[0];
         return winner
+    }
+
+    const checkTie = () => {
+        const allEqual = arr => arr.every(val => val === arr[0]);
+        if (allEqual(possiblePlay) === true && checkWin().length === 0) {
+            display.textContent = 'Tie';
+            return true;
+        } else {
+            return false;
+        };
+    };
+    
+    //Invincible
+    let ai = [];
+    let pre = [];
+    let uniq = [];
+    let uniq2 = [];
+
+    const unbeatable = () => {
+        checkTie();
+        checkWin()
+        winComb.forEach(el => {
+            pre = el.filter(element => playerPlay.includes(element));
+            if(pre.length === 2) {
+                let difference = el.filter(x => !pre.includes(x));
+                ai.push(parseInt(difference.join('')));
+            }
+        })
+        uniq = [...new Set(ai)];        
+        uniq2 = uniq.filter(el => possiblePlay.includes(el));
+
+        if (playerPlay[0] === 5 && playerPlay.length === 1) {
+            square()[2].div.textContent = 'O';
+            delete possiblePlay[2];
+            computerPlay.push(3);
+        } else if (playerPlay[0] === 5 && playerPlay.length > 1){
+            if (uniq2.length > 0) {
+                square()[uniq2[uniq2.length - 1] - 1].div.textContent = 'O';
+                computerPlay.push(uniq2[uniq2.length - 1]);
+                delete possiblePlay[uniq2[uniq2.length - 1] - 1]
+            } else {
+                computer()
+            }
+        }
+
+        if (playerPlay[0] != 5 && playerPlay.length === 1) {
+            square()[4].div.textContent = 'O';
+            delete possiblePlay[4];
+            computerPlay.push(5);
+        } else if (playerPlay[0] != 5 && playerPlay.length > 1){
+            if (uniq2.length > 0) {
+                square()[uniq2[uniq2.length - 1] - 1].div.textContent = 'O';
+                computerPlay.push(uniq2[uniq2.length - 1]);
+                delete possiblePlay[uniq2[uniq2.length - 1] - 1]
+            } else {
+                computer()
+            }
+        }
     }
 
     const computer = () => {
@@ -45,31 +130,37 @@ const Game = () => {
             random.div.textContent = 'O';
             delete possiblePlay[random.value - 1];
             computerPlay.push(random.value);
-            console.log(possiblePlay);
-            console.log(computerPlay);
-        } else if (possiblePlay.indexOf(random.value) === -1 && checkWin().length === 0) {
+        } else if (possiblePlay.indexOf(random.value) === -1 && checkWin().length === 0 && checkTie() === false) {
             computer();
         }
+        return computerPlay;
     }
 
-    const rois = () => {
+    const start = () => {
         square().forEach(el => {
             el.div.addEventListener('click', function() {
-                if (possiblePlay.indexOf(el.value) != -1 && checkWin().length === 0) {
+                if (possiblePlay.indexOf(el.value) != -1 && checkWin().length === 0 && checkTie() === false) {
                     el.div.textContent = 'X';
                     delete possiblePlay[el.value - 1];
-                    playerPlay.push(el.value)
-                    console.log(possiblePlay)
-                    console.log(playerPlay)
-                    computer();
-                }
-            })
-        })
-    }
-    return {rois}
+                    playerPlay.push(el.value);
+                    if (difficulty === 1) {
+                        setTimeout(() => {computer()}, 200)
+                        ///computer();
+                    } else if (difficulty === 2) {
+                        unbeatable();
+                    }
+                    checkWin();
+                    checkTie();
+                    return computerPlay;
+                };
+            });
+        });
+        return
+    };
+
+    return {start}
 }
 
 const player = Game();
 
-player.rois()
-
+player.start()
